39e7eb9e8c9ddb39a637cd90b86c768c06990b95
nnao45 <n4sekai5y@gmail.com>
2019-07-21 14:06:15 +0900

    03

---
+++ exec/03/brigade.js
@@ -0,0 +1,14 @@
+// save this as brigade.js
+const { events, Job } = require('@azure/brigadier')
+
+events.on("exec", (brigadeEvent, project) => {
+  var hello = new Job("linux-job")
+  hello.image = "alpine:3.4"
+  hello.tasks = ["echo Hello Brigade from Azure"]
+  
+  hello.host.name = "virtual-kubelet"
+  hello.resourceRequests.cpu = "1"
+  hello.resourceRequests.memory = "1G"
+  
+  hello.run()
+})
